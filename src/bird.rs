01613bac4215a4a3697a4b907948e73519a0ac49
Bahana Alana Khansa <alanabahana102@gmail.com>
2023-10-25 22:50:27 +0700

    change direction every click

--- src/bird.rs
+++ src/bird.rs
@@ -1,9 +1,10 @@
+use core::ops;
 use bevy::prelude::*;
 
 use crate::{SCALE, WIN_HEIGHT, WIN_WIDTH};
 
 const PRESSED_VELOCITY: f32 = 300.;
-const ACCELERATION_CONSTANT: f32 = 0.001;
+const VELOCITY_CONSTANT: f32 = 0.001;
 const MAX_VELOCITY: f32 = 500.;
 
 pub struct BirdPlugin;
@@ -15,15 +16,26 @@ impl Plugin for BirdPlugin {
     }
 }
 
+#[derive(Clone)]
 enum FlapDirection {
     Vertical,
     Horizontal,
 }
 
+impl ops::Not for FlapDirection {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        match self {
+            FlapDirection::Horizontal => FlapDirection::Vertical,
+            FlapDirection::Vertical => FlapDirection::Horizontal,
+        }
+    }
+}
+
 #[derive(Component)]
 struct Bird {
-    v_vel: f32,
-    h_vel: f32,
+    vel: f32,
     flap: FlapDirection,
 }
 
@@ -37,8 +49,7 @@ fn spawn_bird(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..Default::default()
         },
         Bird {
-            v_vel: 0.,
-            h_vel: 0.,
+            vel: 0.,
             flap: FlapDirection::Vertical,
         },
     ));
@@ -52,38 +63,31 @@ fn move_entity(
 ) {
     let mut berd = q.single_mut();
 
-    if key.just_pressed(KeyCode::W)
+    if key.just_pressed(KeyCode::Space)
         || (mouse.just_pressed(MouseButton::Left) && !mouse.just_pressed(MouseButton::Right))
     {
-        berd.1.v_vel = PRESSED_VELOCITY;
-        berd.1.flap = FlapDirection::Vertical;
-    }
-
-    if key.just_pressed(KeyCode::D)
-        || (mouse.just_pressed(MouseButton::Right) && !mouse.just_pressed(MouseButton::Left))
-    {
-        berd.1.h_vel = PRESSED_VELOCITY;
-        berd.1.flap = FlapDirection::Horizontal;
+        berd.1.vel = PRESSED_VELOCITY;
+        berd.1.flap = !berd.1.flap.clone();
     }
 
     match berd.1.flap {
         FlapDirection::Horizontal => {
-            let move_x = berd.1.h_vel * t.delta().as_millis() as f32 * ACCELERATION_CONSTANT;
+            let move_x = berd.1.vel * t.delta().as_millis() as f32 * VELOCITY_CONSTANT;
             let accl_x = (t.delta().as_millis() as f32).min(MAX_VELOCITY);
-            berd.1.h_vel -= accl_x / 2.;
+            berd.1.vel -= accl_x / 2.;
             berd.0.translation.x = (berd.0.translation.x + move_x)
                 .min(WIN_WIDTH / 2.)
                 .max(-WIN_WIDTH / 2.);
-            berd.1.h_vel -= accl_x / 2.;
+            berd.1.vel -= accl_x / 2.;
         }
         FlapDirection::Vertical => {
-            let move_y = berd.1.v_vel * t.delta().as_millis() as f32 * ACCELERATION_CONSTANT;
+            let move_y = berd.1.vel * t.delta().as_millis() as f32 * VELOCITY_CONSTANT;
             let accl_y = (t.delta().as_millis() as f32).min(MAX_VELOCITY);
-            berd.1.v_vel -= accl_y / 2.;
+            berd.1.vel -= accl_y / 2.;
             berd.0.translation.y = (berd.0.translation.y + move_y)
                 .max(-WIN_HEIGHT / 2.)
                 .min(WIN_HEIGHT / 2.);
-            berd.1.v_vel -= accl_y / 2.;
+            berd.1.vel -= accl_y / 2.;
         }
     }
 }
